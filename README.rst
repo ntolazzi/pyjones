fab6b108fbb256ce9e6b8c514e162bfaa1e51acb
Tolazzi, Nicolas (ntolazzi) <nicolas.tolazzi@mpq.mpg.de>
2017-03-11 11:30:32 +0100

    Prepared everything for upload to Pypi

--- README.rst
+++ README.rst
@@ -3,3 +3,6 @@ PyJones
 
 This package simplifies the calculations of the light polarization for arbitrary
 input polarizations and an unlimited amount of arbitrary optical elements.
+It can easily be installed via
+    pip install pyjones
+The documentation is hosted here or can be build from the docs folder via sphinx.
--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from setuptools import setup, find_packages
 setup(
     name='pyjones',
     version='0.1.0',
-    description='The ultimate package for calculating light polarizations',
+    description='A package for calculating light polarizations',
     author='Nicolas Tolazzi',
     author_email='nicolas.tolazzi@googlemail.com',
     license='MIT',
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['numpy']
+    install_requires=['numpy'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3']
 )

d0b9dae8754c9418d02e2735abf023e572e74e50
Nicolas Tolazzi <nicolas.tolazzi@mpq.mpg.de>
2018-11-16 17:50:53 +0100

    Upgraded version number for Pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjones',
-    version='0.1.0',
+    version='0.2.0',
     description='A package for calculating light polarizations',
     author='Nicolas Tolazzi',
     author_email='nicolas.tolazzi@googlemail.com',
